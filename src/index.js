40bede24798902e53a55d656721bf4a16e6077e2
davenportnw <davenportnw@gmail.com>
2020-09-02 17:22:33 -0500

    Add react bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
