deed829674c4a4496faf0fe2620d18ade84977de
davenportnw <davenportnw@gmail.com>
2020-09-13 17:36:45 -0500

    Update App file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import PortfolioContainer from "./components/PortfolioContainer";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+// import { Nav } from 'react-bootstrap';
 
-import './App.css';
-
-
-const App = () => <PortfolioContainer/>;
+class App extends Component {
+    render() {
+        return(
+            <BrowserRouter>
+            <div>
+                <Navigation/>
+                <Switch>
+                    <Route path="/" component={Home} exact/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/contact" component={Contact}/>
+                    <Route path="/portfolio" component={Portfolio}/>
+                    <Route path="/resume" component={Resume}/>
+                    <Route component={Error}/>
+                </Switch>
+            </div>
+            </BrowserRouter>
+        )
+    }
+}
 
 export default App;
